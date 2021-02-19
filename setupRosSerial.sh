b0f963c6ebe8e89deaa30438ab1dbf39c037b0fd
Dan Bronstein <danbronzy@gmail.com>
2021-02-19 12:31:46 -0500

    FUCK THIS BASH SCRIPT

--- setupRosSerial.sh
+++ setupRosSerial.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+#Takes one optional agument for port to connect to arduino
+#Defaults to /dev/ttyACM0
+#may ask for permission to access port, require password for sudo
+PORT=/dev/ttyACM0
+echo Trying to connect through ${1:-$PORT}
 sudo chmod 666 /dev/ttyACM0
-roscore
-rosrun rosserial_python serial_node.py _port:=/dev/ttyACM0
+gnome-terminal -- /bin/sh -c "roscore"
+sleep 1
+gnome-terminal -- /bin/sh -c "rosrun rosserial_python serial_node.py _port:=${1:-$PORT}"
+#gnome-terminal -- 
 ./sensor_motor_lab_gui
