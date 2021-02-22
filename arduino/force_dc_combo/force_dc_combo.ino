4cc508ee1bd3bb0bff235bc3b277d68f65bd6d96
Ben Kolligs <bkolligs@andrew.cmu.edu>
2021-02-22 12:55:06 -0500

    finished pid controller ready to merge

--- arduino/force_dc_combo/force_dc_combo.ino
+++ arduino/force_dc_combo/force_dc_combo.ino
@@ -1,7 +1,5 @@
 #include <Encoder.h>
 
-
-
 struct abstractMotorSensorPair {
   virtual void setup() = 0;
   virtual void run() = 0;//single loop of read the sensor and set the motor
@@ -12,107 +10,97 @@ struct abstractMotorSensorPair {
   virtual void processGuiCommand(float cmd) = 0;
 };
 
-struct forceResitiveSensorDCPair : abstractMotorSensorPair {
-  // assign pin names
+struct forceDCPair {
+  // pins
   const int FSR_PIN = A0;
   const int I1_PIN = 5;
   const int I2_PIN = 6;
-  const int ENC_PIN_A = 2;
-  const int ENC_PIN_B = 3;
-  
+  const int ENC_A_PIN = 2;
+  const int ENC_B_PIN = 3;
+
   // constant numbers
   const int MS_PER_REV = 674;
   const float VCC = 4.98; // supply voltage
   const float R_DIV = 10000; // resistance in voltage divider
   
-  // calculation numbers
-  bool returnPosition = true;
-  int timePerRev = 0; // time elapsed between encoder revolutions
-  int curTime; // current time since arduino activation
-  long rotorPosition; // current encoder reading
-  float revsPerMin; // revolutions per minute
-  float appliedForce; // force applied to the sensor
-  float sensorOutput; // output of sensor in grams
-  float motorOutput; // motor output either position or RPM
-  
-  // PID parameters
-  float referenceSignal; 
-  float desiredPosition; 
-  float desiredRPM; 
-  float motorInputSignal; 
-  float propGain=0; 
-  float derivGain=0;
-  float integralGain=0; 
-  float errLastTimeStep;
-  float errorAccumulation;
-  Encoder * encoder; // main encoder object
-  
+  // motor stuff
+  long rotorPosition;
+  int curTime;
+  int timePerRev;
+  float revsPerMin;
+  bool positionControl = true;
+
+  // sensor stuff
+  float appliedForce;
+
+  // pid stuff
+  float pGain = 0;
+  float dGain = 0;
+  float iGain = 0;
+  float desiredPosition;
+  float desiredRPM;
+  float prevErr;
+  float errAccumulation;
+  float controlSignal;
+  float scaleInputFactor = MS_PER_REV / 255;
+  float scaleDegreesFactor = MS_PER_REV / 360;
+  Encoder * structEnc;
+
   void setup(){
-    // configure pins correctly
     pinMode(FSR_PIN, INPUT);
     pinMode(I1_PIN, OUTPUT);
     pinMode(I2_PIN, OUTPUT);
-    
-    // Encoder
-    Encoder enc(ENC_PIN_A, ENC_PIN_B);
-    encoder = &enc;
+    Encoder enc(ENC_A_PIN, ENC_B_PIN);
+    structEnc = & enc;
   }
 
-  // main running function
   void run(){
-    // output is different depending on what the value of returnPosition is
-    motorOutput = motorFeedback();
-    sensorOutput = sensorFeedback();
+    float reference;
+    float current;
+  
+    calculatePosVel();
+    calculateForce();
 
-    // select position or velocity as reference
-    if (returnPosition){
-      referenceSignal = desiredPosition; // degrees
+    
+    if (positionControl){
+        reference = desiredPosition;
+        current = rotorPosition;
     }
     else{
-      referenceSignal = desiredRPM; // RPM
+        reference = desiredRPM;
+        current = revsPerMin;
+        desiredPosition = appliedForce;
+        desiredRPM = appliedForce;
     }
 
-    float err = referenceSignal - motorOutput;
-    float errdot = (err - errLastTimeStep);
-    errorAccumulation += err;
+    float err = reference - current;
+    Serial.print("Position: ");
+    Serial.println(rotorPosition);
+    Serial.print("RPM: ");
+    Serial.println(revsPerMin);
+    Serial.print("Error: ");
+    Serial.println(err);
+    float errDot = err - prevErr;
+    errAccumulation += err;
+    prevErr = err;
 
-    motorInputSignal = propGain*err + derivGain*errdot + integralGain*errorAccumulation;
-
-    sendInputToMotor(motorInputSignal);
-    errLastTimeStep = err;
+    controlSignal = pGain*err + dGain*errDot + iGain*errAccumulation;
+    sendMotorInput(controlSignal);
   }
 
-  void setMotionGains(float Kp, float  Kd, float Ki, float positionDes, float speedDes){
-    propGain = Kp;
-    derivGain = Kd;
-    integralGain = Ki;
-    desiredPosition = positionDes;
-    desiredRPM = speedDes;
+  
+  void calculatePosVel(){
+    rotorPosition = abs(structEnc->read());
+    
+    // calculate RPM
+    if (rotorPosition >= MS_PER_REV){
+      structEnc->write(0);
+      timePerRev = millis() - curTime;
+      curTime = millis();
+      revsPerMin = 1 / (timePerRev / (1000.0 * 60));
+    }
   }
   
-  // feedback for motor, can return speed or position
-  float motorFeedback(){
-    calculatePosVel();
-    
-    if (returnPosition){
-      return rotorPosition;
-    }
-    else {
-      return revsPerMin;
-    }
-    
-  }
-
-  // feedback from sensor
-  float sensorFeedback(){
-    calculateForce();
-    return appliedForce;
-  }
-
-  void processGuiCommand(float cmd){
-    // nothing here yet
-  }
-
   // calculate force from the sensor
   void calculateForce(){
     int frsADC; // the raw analog to digital converter reading
@@ -142,45 +130,68 @@ struct forceResitiveSensorDCPair : abstractMotorSensorPair {
     }
   }
 
-  // calculate motor position and velocity
-  void calculatePosVel(){
-    rotorPosition = encoder->read();
-    if (rotorPosition >= MS_PER_REV){
-      encoder->write(0);
-      timePerRev = millis() - curTime;
-      curTime = millis();
-      revsPerMin = 1 / (timePerRev / (1000.0 * 60));
-    }
-  }
-
-  void sendInputToMotor(float input){
-    // input to PWM range 0-255
-    int inputSignal = (int) input/255;
-
-    // set spin direction
-    if (inputSignal >= 0){
-      analogWrite(I1_PIN, inputSignal);
+  void sendMotorInput(float speed){
+    int input = (int) speed/scaleInputFactor;
+    Serial.print("Input Signal: ");
+    Serial.println(input);
+    
+    if (input >= 0){
+      if (abs(input) > 10)
+      {input = constrain(input, 70, 255);
+      analogWrite(I1_PIN, abs(input));
       digitalWrite(I2_PIN, LOW);
+      }
+      else{
+        digitalWrite(I1_PIN, LOW);
+        digitalWrite(I2_PIN, LOW);
+      }
     }
-    else{
-      digitalWrite(I1_PIN, LOW);
-      analogWrite(I2_PIN, inputSignal);
+    else {
+      if (abs(input) > 10){
+        input = constrain(input, -255, -70);
+        digitalWrite(I1_PIN, LOW);
+        analogWrite(I2_PIN, abs(input));
+      }
+      else{
+        digitalWrite(I1_PIN, LOW);
+        digitalWrite(I2_PIN, LOW);
+      }
     }
+
   }
-  
+
+  void setMotionGains(float Kp, float  Kd, float Ki, float positionDes, float speedDes, bool posCtrl){
+    pGain = Kp;
+    dGain = Kd;
+    iGain = Ki;
+    desiredPosition = positionDes;
+    desiredRPM = speedDes;
+    positionControl = posCtrl;
+  }
+
+  float motorFeedback(){
+    return rotorPosition;
+  }
+  float sensorFeedback(){
+    return appliedForce;
+  }
+
+  void processGuiCommand(float cmd){
+    
+  }
+
 };
 
-// now the test program
-forceResitiveSensorDCPair frs;
+forceDCPair frs;
 
 void setup() {
-  Serial.begin(9600);
-  frs.setup();
-  frs.setMotionGains(1, 1, 0, 90, 40);
-
+    Serial.begin(9600);
+    frs.setup();
+    frs.setMotionGains(1, 1, 0, 270, 40, false);
 }
 
 void loop() {
   frs.run();
-
+//      digitalWrite(frs.I1_PIN, LOW);
+//      digitalWrite(frs.I2_PIN, LOW);
 }
